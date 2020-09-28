cc2928d50473b86e6e5f41c75b00b2736b2bce09
luxor94 <47752490+luxor94@users.noreply.github.com>
2020-09-28 21:33:53 +0300

    feat: what-seasin

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(data) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let d = data.getMonth() + 1;  
+  let a = new Date(2150, 12, 21, 18, 36, 41, 841)
+  if (d == null) {
+    return null;
+  } else {
+
+  if (d < 3) {
+    return 'winter';
+    }
+    if  (d < 6) {
+    return 'spring';
+    }
+    if (d < 9) {
+    return 'summer';
+    }
+    if (d < 11) {
+    return 'autumn';
+    }
+    if ( d = 12) {
+    return 'winter';
+    }
+  }
 };
