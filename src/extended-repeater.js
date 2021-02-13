d180a80179aa432be1142cc1e88d2666c2e1317a
luxor94 <47752490+luxor94@users.noreply.github.com>
2021-02-13 14:31:54 +0300

    task repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newStr = str;
+  let separators = options.separator
+  let additions = options.addition
+  let additionRepeatTime = options.additionRepeatTimes
+  let additionSeparators = options.additionSeparator
+
+  if (separators == null) {
+    separators = '+'
+  } else {
+    separators
+  }
+  if (additions == null) {
+    newStr = str +''
+    additions = ''
+  } else {
+    newStr = str + additions
+  }
+
+  let newAddition = additions
+
+  if (additionSeparators == null) {
+    additionSeparators = '|'
+  } else {
+    additionSeparators = additions + additionSeparators
+  }
+
+  
+
+  for (let i = 0; i < additionRepeatTime-1; i++) {
+    newAddition += `${additionSeparators}${newAddition}`
+  }
+
+
+  
+      for(let i = 0; i < options.repeatTimes-1; i++) {
+      newStr += `${separators}${str}${newAddition}`
+      
+    }
+    return newStr
+    
+
 };
   
