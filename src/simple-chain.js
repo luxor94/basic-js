50180f30df49f13d3fbe6cdfb2ee92dade1af45b
luxor94 <47752490+luxor94@users.noreply.github.com>
2021-03-20 16:10:43 +0300

    feat: task simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,19 +6,45 @@ const chainMaker = {
     return this.chain.length;
   },
   addLink(value) {
-    this.chain.push(` ( ${value} ) `);
-    return chain;
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    return chain;
+    if (this.checkNum(position)) {
+      if (position < 1 || position > this.getLength()) {
+        this.delChain();
+        throw new Error('throws an Error on removing wrong link');
+      }
+      const ind = position - 1;
+      this.chain.splice(ind,1);
+      return this;
+    }
+    this.delChain();
+    throw new Error('throws an Error on removing wrong link');
   },
   reverseChain() {
     this.chain.reverse();
-    return this
+    return this;
   },
   finishChain() {
-    return chain;
+    const finishChain = this.getChain();
+    this.delChain()
+    return finishChain;
+  },
+
+  delChain() {
+    this.chain.length = 0;
+
+  },  
+  
+  checkNum() {
+    return typeof num == 'number' && (num ^ 0) == num;
+  },
+
+  getChain() {
+    return this.chain.join('~~')
   }
+
 };
 
 module.exports = chainMaker;
