4e423341137364c7b506e33b7b81dbf0abc8a422
luxor94 <47752490+luxor94@users.noreply.github.com>
2021-03-21 10:03:35 +0300

    feat: vigener decriptor

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,6 +5,7 @@ class VigenereCipheringMachine {
     value = value.toUpperCase();
     code = code.toUpperCase();
     code = code.repeat(Math.ceil(value.length/code.length))
+    code = code.slice(0, value.length);
     const enStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let str = [];
     let j = 0
@@ -35,9 +36,41 @@ class VigenereCipheringMachine {
     return result.join('')
 
   }    
-  // decrypt() {
+  decrypt(value, code) {
+    value = value.toUpperCase();
+    code = code.toUpperCase();
+    code = code.repeat(Math.ceil(value.length/code.length));
+    code = code.slice(0, value.length);
+    const enStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let str = [];
+    let j = 0
+    for(let i = 0; i < value.length; i++) {
 
-  // }
+      if (enStr.indexOf(value[i]) >= 0) {
+        if (enStr.indexOf(value[i]) - enStr.indexOf(code[i-j]) >= 0) {
+          str.push(enStr.indexOf(value[i]) - enStr.indexOf(code[i-j]))
+        }
+        if (enStr.indexOf(value[i]) - enStr.indexOf(code[i-j]) < 0) {
+          str.push(enStr.indexOf(value[i]) - enStr.indexOf(code[i-j])+26)
+        }
+      } else {
+        str.push(value[i])
+        j++
+        continue
+      }
+    }
+
+    let result = []
+    for (let i = 0; i < str.length; i++) {
+      if (enStr.indexOf(value[i]) >= 0) {
+        result.push(enStr[str[i]])
+      } else {
+        result.push(value[i])
+      }
+      
+    }
+    return result.join('')
+  }
 }
 
 module.exports = VigenereCipheringMachine;
